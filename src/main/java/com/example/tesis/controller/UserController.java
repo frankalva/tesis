c24b48361b8e809ee235c6d2c69172ff01418450
Frank Alva <frankalva08@gmail.com>
2024-11-06 14:48:48 -0500

    fix register

--- src/main/java/com/example/tesis/controller/UserController.java
+++ src/main/java/com/example/tesis/controller/UserController.java
@@ -3,6 +3,8 @@ package com.example.tesis.controller;
 import com.example.tesis.model.User;
 import com.example.tesis.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,8 +15,9 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public User registerUser(@RequestBody User user){
-        return userService.registerUser(user);
+    public ResponseEntity<User> registerUser(@RequestBody User user){
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+
     }
 
     @GetMapping("/getEmail")
